2f2813e3e7608d0d6e715b757ca964125c39c435
ASHISH KUMAt <akumaraman966@gmail.com>
2022-04-13 15:14:34 +0530

    update
    updated

--- script.js
+++ script.js
@@ -1,78 +1,86 @@
 
 
 // creat a object that hold the weather data;
-let weather = {
-      apiKey: "83726eb96b2f5b24f1aa10b19fa264c4", 
-      apidata:function (name ="Delhi") {   
+// let weather = {
+//       apiKey: "83726eb96b2f5b24f1aa10b19fa264c4", 
+//       apidata:function () {   
+//            let getdata = fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+//                   name +
+//                   "&units=metric&appid=" +
+//                   this.apiKey)
+//                let jsondata= getdata.then(weatherdata => weatherdata.json());
 
-           let getdata = fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-                  name +
-                  "&units=metric&appid=" +
-                  this.apiKey)
+//                   return(
+//                          jsondata.then(finaldata => console.log(finaldata))
+//                   )
+//       }
+// }
 
-               let jsondata= getdata.then(weatherdata => weatherdata.json());
 
-                  return(
-                         jsondata.then(finaldata => console.log(finaldata))
-                  )
-      }
-}
+
+ // let's create web api 
+
+
+  let weatherObj = {
+         apiKey : "83726eb96b2f5b24f1aa10b19fa264c4",
+         
+
+  }
 
 
  console.log(weather.apidata())
 
 
-                  // handling success 
+                
+                  apidata.then(response => response.json())  // convert to json
+                  .then(data => {
+                        //    
+                        const { name } = data;
+                        const { icon, description } = data.weather[0];
+                        const { temp, humidity, temp_min, temp_max } = data.main;
+                        const { speed } = data.wind;
+                        const { visibility } = data;
 
-//                   apidata.then(response => response.json())  // convert to json
-//                   .then(data => {
-//                         //    
-//                         const { name } = data;
-//                         const { icon, description } = data.weather[0];
-//                         const { temp, humidity, temp_min, temp_max } = data.main;
-//                         const { speed } = data.wind;
-//                         const { visibility } = data;
+                        /*sending data to website*/
 
-//                         /*sending data to website*/
+                        document.getElementById("city").innerHTML = `Weather in ${name}`; //update city name
+                        document.getElementById("weather-icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+                        document.getElementsByClassName("temp")[0].innerHTML = "Temperature " + temp + " °C";
+                        document.getElementsByClassName("temp")[1].innerHTML = "Wind " + speed + " Km/S";
+                        document.getElementsByClassName("tem-des")[0].innerHTML = description;
+                        document.getElementsByClassName("tem-des")[1].value = humidity + "%";
+                        document.getElementById("today").innerHTML = today
+                        document.getElementsByClassName("min-temp")[0].innerHTML = "Min  Temp 👉 " + temp_min;
+                        document.getElementsByClassName("min-temp")[1].innerHTML = " Max Temp  👉 " + temp_max;
 
-//                         document.getElementById("city").innerHTML = `Weather in ${name}`; //update city name
-//                         document.getElementById("weather-icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-//                         document.getElementsByClassName("temp")[0].innerHTML = "Temperature " + temp + " °C";
-//                         document.getElementsByClassName("temp")[1].innerHTML = "Wind " + speed + " Km/S";
-//                         document.getElementsByClassName("tem-des")[0].innerHTML = description;
-//                         document.getElementsByClassName("tem-des")[1].value = humidity + "%";
-//                         document.getElementById("today").innerHTML = today
-//                         document.getElementsByClassName("min-temp")[0].innerHTML = "Min  Temp 👉 " + temp_min;
-//                         document.getElementsByClassName("min-temp")[1].innerHTML = " Max Temp  👉 " + temp_max;
-
-//                   })
-
+                  })
 
 
 
 
 
-//                   .catch(() => {
-//                         document.getElementById("city").innerHTML = " Oh 😩!! Please search for a valid city ";
-//                   });// Catch errors.
+
+                  .catch(() => {
+                        document.getElementById("city").innerHTML = " Oh 😩!! Please search for a valid city ";
+                  });// Catch errors.
 
 
-//       }
+      }
 
-// }
+}
 
 
 
-// function updateData() {
-//       let inputCity = document.getElementById("cityName").value;
-//       const fithData = weather.fetchWeatherData(inputCity);
+function updateData() {
+      let inputCity = document.getElementById("cityName").value;
+      const fithData = weather.fetchWeatherData(inputCity);
 
-// }
+}
 
-// function myFunction() {
+function myFunction() {
 
-//       const fithData = weather.fetchWeatherData("PATNA");
-// }
+      const fithData = weather.fetchWeatherData("PATNA");
+}
 
 
 let today = new Date().toISOString().slice(0, 10);
